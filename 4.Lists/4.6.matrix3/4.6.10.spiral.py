10f908f10b09244f285d445a810923db4050ab1f
gennady <genakoganovich@gmail.com>
2023-07-07 13:49:38 +0300

    0018: 4.6.10.spiral.py start2

--- 4.Lists/4.6.matrix3/4.6.10.spiral.py
+++ 4.Lists/4.6.matrix3/4.6.10.spiral.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def print_matrix(matrix, width=1):
     for r in range(len(matrix)):
         for c in range(len(matrix[0])):
@@ -6,13 +9,7 @@ def print_matrix(matrix, width=1):
 
 
 def populate_matrix(matrix):
-    count = 0
-    for offset in range(len(matrix) - 1, -len(matrix[0]), -1):
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i + j + 1 == len(matrix) - offset:
-                    count += 1
-                    matrix[i][j] = count
+
     return matrix
 
 
@@ -20,4 +17,14 @@ def run(n, m):
     print_matrix(populate_matrix([[0] * m for _ in range(n)]), 3)
 
 
-run(*(map(int, input().split())))
+# run(*(map(int, input().split())))
+
+matrix = np.array(np.arange(12).reshape((3, 4)))
+while matrix.size:
+    print(matrix)
+    if min(matrix.shape) > 1:
+        for _ in range(3):
+            matrix = np.rot90(matrix)
+            print(matrix)
+
+    matrix = matrix[1:-1, 1:-1]
